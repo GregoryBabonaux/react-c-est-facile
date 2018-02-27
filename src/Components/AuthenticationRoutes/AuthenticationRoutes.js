e1c9254c813970cc77c39694220dca63dfa7e78f
Daibai <gregory@babonaux.com>
2018-02-27 11:31:40 +0100

    Add ProtectedRoutes and AuthenticationRoutes

---
+++ src/Components/AuthenticationRoutes/AuthenticationRoutes.js
@@ -0,0 +1,27 @@
+import React  from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import * as stuffActions from '../../Services/Stuff/actions'
+import {Redirect} from 'react-router-dom'
+
+import Layout from '../Layout/Layout'
+
+const ProtectedRoutes = ({component: Component, ...rest}) => {
+  const {path} = rest;
+  const {logged} = rest.auth
+
+  if( logged ){
+    return <Redirect to="/" />
+  }
+
+  return (
+    <Layout exact path={path} component={Component} />
+  )
+}
+
+
+const mapStateToProps = ({auth}) => ({
+  auth
+})
+
+export default connect(mapStateToProps)(ProtectedRoutes);
